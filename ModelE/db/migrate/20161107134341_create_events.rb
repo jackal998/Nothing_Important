d1be5b7aa59f482713a01c48415930b7ff93c9ea
jackal998 <jackal998@gmail.com>
2016-11-07 21:54:04 +0800

    event_cat..._db:migrate

--- ModelE/db/migrate/20161107134341_create_events.rb
+++ ModelE/db/migrate/20161107134341_create_events.rb
@@ -1,6 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
+    	t.string :name
+    	t.integer :capacity
+    	t.string :description
 
       t.timestamps
     end
