6bfaf21bf9259f18b7a11316ed563bb24e582df9
jackal998 <jackal998@gmail.com>
2016-11-07 21:59:05 +0800

    attendee_db:migrate

--- ModelE/db/schema.rb
+++ ModelE/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107134351) do
+ActiveRecord::Schema.define(version: 20161107135809) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
