8aafe237d0e2dad6892da5c267874d7df4dd2b0e
jackal998 <jackal998@gmail.com>
2016-11-08 15:48:53 +0800

    event_attendee_create

---
+++ ModelE/app/controllers/event_attendees_controller.rb
@@ -0,0 +1,40 @@
+class EventAttendeesController < ApplicationController
+
+	before_action :find_event
+
+	def index
+		@attendees = @event.attendees
+	end
+	def new
+		@attendee =  @event.attendees.new
+	end
+	def create
+		@attendee =  @event.attendees.new(attendee_params)
+		if @attendee.save
+			flash[:notice] = "新增成功"
+			redirect_to :action => :index
+		else
+		end
+	end
+	def show
+		
+	end
+	def edit
+		
+	end
+	def update
+		
+	end
+	def destroy
+		
+	end
+
+	private
+	def attendee_params
+		params.require(:attendee).permit(:name)
+	end
+
+	def find_event
+		@event = Event.find(params[:event_id])
+	end
+end
